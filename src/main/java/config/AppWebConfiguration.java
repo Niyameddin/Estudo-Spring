a712bc4b0defecc48ec40c5ec892d31ab61db77b
Guilherme Alves <gsag.dh@gmail.com>
2015-08-24 22:57:54 -0300

    Adicionando configurações no projeto
    Adicionando configurações no projeto

--- src/main/java/config/AppWebConfiguration.java
+++ src/main/java/config/AppWebConfiguration.java
@@ -7,14 +7,17 @@ package config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"controller","entity","persistence","util"})
-public class AppWebConfiguration {
+public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 
     /*
      * Configure View Resolver
@@ -30,6 +33,7 @@ public class AppWebConfiguration {
     /*
     * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
     */
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("/static/");
     }
